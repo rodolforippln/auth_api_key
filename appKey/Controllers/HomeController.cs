f2acddf9fe0b09de814cdf3213bf397b23b00b02
Rodolfo <rodolforippln@gmail.com>
2023-04-17 00:44:37 -0300

    first commit

---
+++ appKey/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using appKey.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace appKey.Controllers;
+
+[ApiController]
+[Route("")]
+public class HomeController : ControllerBase
+{
+
+    [HttpPost("")]
+    [ApiKey]
+    public IActionResult Get()
+    {
+        return Ok("Olá mundo!!");
+    }
+}
